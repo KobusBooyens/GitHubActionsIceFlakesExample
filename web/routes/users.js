2c0f62d7d59bb5312eeddc62df1676fbf3ab5aed
KobusBooyens <60418701+KobusBooyens@users.noreply.github.com>
2023-09-14 18:57:42 +0200

    Update users.js
    Added route to get user by id

--- web/routes/users.js
+++ web/routes/users.js
@@ -6,4 +6,8 @@ router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
 
+router.get('/users:id', function (req, res, next) {
+  res.send('respond with a resource')
+})
+
 module.exports = router
